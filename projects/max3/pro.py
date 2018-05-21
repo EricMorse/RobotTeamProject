2410407dbad87983b0442ac88d66bdf03126772b
nebulaenb <max3@rose-hulman.edu>
2018-05-20 22:04:33 -0400

    change

---
+++ projects/max3/pro.py
@@ -0,0 +1,14 @@
+"""
+    This module is my final project. It
+"""
+import tkinter
+import tkinter.messagebox
+import random
+
+
+root = tkinter.Tk()
+root.title('Rabbit and Turtle')
+root.geometry("400x400+550+200")
+
+root.mainloop()
+

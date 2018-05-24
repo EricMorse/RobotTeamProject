99568a3eefdabb279b260544ff130d8a4bd800d0
nebulaenb <max3@rose-hulman.edu>
2018-05-24 10:17:37 -0400

    change

--- projects/max3/max3_ev3.py
+++ projects/max3/max3_ev3.py
@@ -2,16 +2,17 @@ import mqtt_remote_method_calls as com
 import robot_controller as robo
 import ev3dev.ev3 as ev3
 
+
 def main():
     robot = robo.Snatch3r()
     mqtt_client = com.MqttClient(robot)
     mqtt_client.connect_to_pc()
 
- #   if robot.touch_sensor.is_pressed:
-  #      robot.arm_up()
-   #     if robot.touch_sensor.is_pressed:
-    #        ev3.Sound.speak("Thank you! Bye bye!").wait()
-     #       robot.shutdown()
+    if robot.touch_sensor.is_pressed:
+        robot.arm_up()
+        ev3.Sound.speak("Thank you!").wait()
+        robot.shutdown()
+
     robot.loop_forever()
 
 
